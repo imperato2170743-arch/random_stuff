1db15a48abca61f82648d6b9e6ab5c6f5f9b08e1
imperato2170743-arch <imperato.2170743@studenti.uniroma1.it>
2026-07-05 09:04:06 +0200

    Remove 'angry' and 'tired' buttons
    Removed buttons for 'angry' and 'tired' emotions.

--- Hello world 2 .py
+++ Hello world 2 .py
@@ -15,6 +15,3 @@ if st.button('happy'):
     st.balloons()
 if st.button('sad'):
     st.image(media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3a25oYzl3Z3BxNzJpaWh0MTRyd3R4OXQ4bzI5NHlwMzByejRyOTY1ayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Yle9Yz9izeVRyiwavn/giphy.gif)
-if st.button('angry'):
-
-if st.button('tired'):
