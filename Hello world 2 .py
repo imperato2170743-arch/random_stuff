323867175f6d4909fce5bc0c3c05c5f6d728e7b7
imperato2170743-arch <imperato.2170743@studenti.uniroma1.it>
2026-07-05 09:09:24 +0200

    Refactor greeting logic and add button functionality

--- Hello world 2 .py
+++ Hello world 2 .py
@@ -1,17 +1,19 @@
 import streamlit as st
 
-
-
+col1, col2, col3, col4 = st.columns(4)
 
 st.title("Hello world")
 
 name = st.text_input("Hello friend what's your name?")
-if name == 'Ruben':
-    st.write(f'Nice to meet you, {name}!')
-else :
-    st.write(f'access denied')
 
-if st.button('happy'):
+
+if name == 'Ruben':
+   st.write(f'Nice to meet you, {name}!')
+else :
+   st.write(f'access denied')
+with col1
+   if st.button('happy'):
     st.balloons()
-if st.button('sad'):
+with col2 
+   if st.button('sad'):
     st.image("https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3a25oYzl3Z3BxNzJpaWh0MTRyd3R4OXQ4bzI5NHlwMzByejRyOTY1ayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Yle9Yz9izeVRyiwavn/giphy.gif")
