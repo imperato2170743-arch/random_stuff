c04a92fc449d76e7088bb7654f01acf8d7e17d94
imperato2170743-arch <imperato.2170743@studenti.uniroma1.it>
2026-07-05 09:03:12 +0200

    Fix typo in button condition for 'tired'

--- Hello world 2 .py
+++ Hello world 2 .py
@@ -17,4 +17,4 @@ if st.button('sad'):
     st.image(https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3a25oYzl3Z3BxNzJpaWh0MTRyd3R4OXQ4bzI5NHlwMzByejRyOTY1ayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Yle9Yz9izeVRyiwavn/giphy.gif)
 if st.button('angry'):
 
-if st.button(tired'):
+if st.button('tired'):
