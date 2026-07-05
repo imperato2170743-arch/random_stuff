c4434e67646371d7090b0babe83e7ba26d8bcaf8
imperato2170743-arch <imperato.2170743@studenti.uniroma1.it>
2026-07-05 08:48:49 +0200

    Fix formatting in Hello world 2.py

--- Hello world 2 .py
+++ Hello world 2 .py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.title("Hello world")
 
-name = st.text_input("Hello friend what's your name?", font = 'helvetica,16')
+name = st.text_input("Hello friend what's your name?")
 if name == 'Ruben':
     st.write(f'Nice to meet you, {name}!')
 else :
