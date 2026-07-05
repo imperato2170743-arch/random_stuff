6d8d2c759e491b9904a3d07b2b9ce94c71c6f3ae
imperato2170743-arch <imperato.2170743@studenti.uniroma1.it>
2026-07-05 08:48:11 +0200

    Fix syntax error in else statement

--- Hello world 2 .py
+++ Hello world 2 .py
@@ -8,4 +8,5 @@ st.title("Hello world")
 name = st.text_input("Hello friend what's your name?", font = 'helvetica,16')
 if name == 'Ruben':
     st.write(f'Nice to meet you, {name}!')
-else st.write(f'access denied')
+else :
+    st.write(f'access denied')
