897aaa9e1aac4b62d1411bd44ab6dd6219a53b18
imperato2170743-arch <imperato.2170743@studenti.uniroma1.it>
2026-07-05 09:29:43 +0200

    Fix equality checks for button clicks

--- Hello world 2 .py
+++ Hello world 2 .py
@@ -27,12 +27,12 @@ with col2:
    if st.button('tired'):
     tired_click = True
 
-if happy_click = True :
+if happy_click == True :
     st.balloons()
-if fine_click = True :
+if fine_click == True :
     st.image('https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGFtaXlvMHFiMjE5b2RpOGZ3ZXYzdHVtYmRkZWFhbmVwdTlhN2FlcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1FkCqpyObTuo0/giphy.gif',width= 500)
 
-if sad_click = True:
+if sad_click == True:
     st.image("https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3a25oYzl3Z3BxNzJpaWh0MTRyd3R4OXQ4bzI5NHlwMzByejRyOTY1ayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Yle9Yz9izeVRyiwavn/giphy.gif",width=500)
-if tired_click = True:
+if tired_click == True:
     st.image("https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3MnQxNzcyNzNibWFpOWY5d3RoNXluMmo4enk1Znc1dmkzenR3cHl5MCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/A8ReUjJdMCNOM/giphy.gif",width=500)
