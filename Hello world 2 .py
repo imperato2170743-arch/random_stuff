ecbb593e0ada7ab203a76eaddc35255ccf876785
imperato2170743-arch <imperato.2170743@studenti.uniroma1.it>
2026-07-05 09:09:46 +0200

    Fix indentation and update button actions

--- Hello world 2 .py
+++ Hello world 2 .py
@@ -11,9 +11,9 @@ if name == 'Ruben':
    st.write(f'Nice to meet you, {name}!')
 else :
    st.write(f'access denied')
-with col1
+with col1:
    if st.button('happy'):
     st.balloons()
-with col2 
+with col2:
    if st.button('sad'):
     st.image("https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3a25oYzl3Z3BxNzJpaWh0MTRyd3R4OXQ4bzI5NHlwMzByejRyOTY1ayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/Yle9Yz9izeVRyiwavn/giphy.gif")
